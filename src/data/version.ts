42a5894729cf265fcd40a67fa5e4b047201272ce
bryanjosephcruz06-afk <bryanjosephcruz06@gmail.com>
2026-05-20 10:58:26 +0800

    add version file fix

--- src/data/version.ts
+++ src/data/version.ts
@@ -1,4 +1,4 @@
 import { PATCH_NOTES } from "./patchNotes";
 
 export const VERSION =
-  PATCH_NOTES?.length > 0 ? PATCH_NOTES[0]?.version ?? "unknown" : "unknown";
+  PATCH_NOTES?.[0]?.version ?? "unknown";
