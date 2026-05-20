854354e60fa20d73a0032b0ede871db6565d1db3
bryanjosephcruz06-afk <bryanjosephcruz06@gmail.com>
2026-05-20 10:56:20 +0800

    Update version.ts

--- src/data/version.ts
+++ src/data/version.ts
@@ -1,3 +1,4 @@
-import { PATCH_NOTES } from "@/data/patchNotes";
+import { PATCH_NOTES } from "./patchNotes";
 
-export const VERSION = PATCH_NOTES.length > 0 ? PATCH_NOTES[0].version : "unknown";
+export const VERSION =
+  PATCH_NOTES?.length > 0 ? PATCH_NOTES[0]?.version ?? "unknown" : "unknown";
